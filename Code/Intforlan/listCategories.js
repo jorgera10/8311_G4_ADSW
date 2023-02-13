7f908878511186926ce04b16342317650b3e708a
JsRamos_33 <jorgeramosgc@gmail.com>
2023-02-13 13:56:02 -0500

    app

---
+++ Code/Intforlan/listCategories.js
@@ -0,0 +1,27 @@
+
+export const listCategories = [
+  {
+    id: 1,
+    category: "Computadoras",
+    description:
+      " ¿Necesitas una computadora? Aquí en Intforlan encontraras las mejores computadoras del mercado a un precio increible, ¡Que esperas para llevarte la tuya!",
+  },
+  {
+    id: 2,
+    category: "Monitores",
+    description:
+      "Te traemos las mejores modelos en Monitores LED FULL HD para que puedas disfrutar de tus vídeos, películas y juegos en alta definición...",
+  },
+  {
+    id: 3,
+    category: "Celulares",
+    description:
+      " Todo lo que necesitas al alcance de tus manos, la mejor tecnologia en celulares inteligentes ideales para recibir clases o convertirte en un gamer profesional",
+  },
+  {
+    id: 4,
+    category: "Laptop",
+    description:
+      " ¿Necesitas una computadora? Aquí en Intforlan encontraras las mejores computadoras del mercado a un precio increible, ¡Que esperas para llevarte la tuya!",
+  },
+];
